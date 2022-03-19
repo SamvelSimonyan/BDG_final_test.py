b49e6ab05dd1a7086008ad9535a9ec10135a247b
SamvelSimonyan <samvelsimonyan25@gmail.com>
2022-03-19 18:29:54 +0400

    v 0.1

--- go_to_url.py
+++ go_to_url.py
@@ -1,91 +1,26 @@
-import selenium
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.keys import Keys
-import time
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-import random
+from defs import *
 
-def email_generator(name):
-    r = random.randint(10000,99999)
-    email = name + str(r) + "@gmail.com"
-    print(email)
-    return email
-
-first_name = "Samvel"
-last_name = "Simonyan"
-phone = "123456789"
-email = email_generator("Samvel")
-password = email
-hotel_location = "Yerevan"
-base_url = "https://www.phptravels.net/signup"
-
-
-driver = webdriver.Chrome("chromedriver")
-driver.implicitly_wait(10)
-driver.maximize_window()
-action = ActionChains(driver)
 driver.get(base_url)
+to_english()
+driver.get(base_url)
+sign_up()
+login()
+click_a_hotel(hotel_location, 0)
+room_list = get_room_list()
 
-
-driver.find_element_by_xpath('//*[@placeholder="First Name"]').send_keys(first_name)
-driver.find_element_by_xpath('//*[@placeholder="Last Name"]').send_keys(last_name)
-driver.find_element_by_xpath('//*[@placeholder="Phone"]').send_keys(phone)
-driver.find_element_by_xpath('//*[@placeholder="Email"]').send_keys(email)
-driver.find_element_by_xpath('//*[@placeholder="Password"]').send_keys(password)
-
-driver.find_element_by_xpath('//*[@id="fadein"]/div[1]/div/div[2]/div[2]/div/form/div[7]/button').click()
-
-driver.find_element_by_xpath('//*[@placeholder="Email"]').send_keys(email)
-driver.find_element_by_xpath('//*[@placeholder="Password"]').send_keys(password)
-
-
-driver.find_element_by_xpath('//*[@id="fadein"]/div[1]/div/div[2]/div[2]/div/form/div[3]/button').click()
-driver.find_element_by_xpath('//*[@id="fadein"]/header/div[2]/div/div/div/div/div[2]/nav/ul/li[2]/a').click()
-
-
-search = driver.find_element_by_xpath('//*[@id="select2-hotels_city-container"]')
-action.move_to_element(search).click().perform()
-
-driver.find_element_by_class_name('select2-search__field').send_keys(hotel_location)
-
-time.sleep(3)
-driver.find_element_by_class_name('select2-search__field').send_keys(Keys.ENTER*1)
-
-driver.find_element_by_xpath('//*[@id="submit"]').click()
-driver.find_element_by_xpath('//*[@id="caucasus hotel"]/div/div[2]/div/div[2]/div/a/span[1]').click()
-
-driver.switch_to.window(driver.window_handles[1])
-driver.find_element_by_xpath('//*[@id="SearchBoxContainer"]/div/div/button/span').click()
-
-
-try:
-    driver.find_element_by_xpath('/html/body/div[16]/div/div/i').click()
-except NoSuchElementException:
+for room in room_list:
     try:
-        driver.find_element_by_xpath('/html/body/div[15]/div/div/i').click()
+        room.click()
+        add_to_card()
     except NoSuchElementException:
-      pass
+        driver.close()
+        driver.switch_to.window(driver.window_handles[1])
+        continue
+    else:
+        break
 
-
-driver.find_element_by_xpath('//*[@id="contentContainer"]/div[3]/ol/li[1]/div[2]/a/div/div[3]/div/div[3]/button/div/div/div/span').click()
-driver.switch_to.window(driver.window_handles[2])
-
-driver.find_element_by_xpath("//*[contains(@id,'BGgQoBjAB')]/div/div[5]/div[1]/div/div[1]/div/button/div/div/span").click()
-driver.find_element_by_xpath('//*[@id="roomGrid"]/div[5]/div[2]/div[1]/div[4]/button/div/div/span').click()
-
-
-
-driver.find_element_by_xpath('//*[@id="firstName_lastName"]').send_keys(first_name+" "+last_name)
-driver.find_element_by_xpath('//*[@id="email"]').send_keys(email)
-time.sleep(1)
-driver.find_element_by_xpath('//*[@id="retypeEmail"]').send_keys(email)
-time.sleep(1)
-driver.find_element_by_xpath('//*[@id="SiteContent"]/div/div[1]/div[5]/div/button/div/div/span').click()
-
+customer_info_filler()
 url = driver.current_url
-
 
 print(url)
 true_url = "https://www.agoda.com/book/payment/"
